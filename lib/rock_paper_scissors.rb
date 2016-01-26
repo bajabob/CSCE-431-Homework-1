9d33c807e33d0f5d6e139625f472e86d2e9154d8
BajaBob <bajabob.tx@gmail.com>
2016-01-26 16:37:54 +0000

    rock paper sissors completed

--- lib/rock_paper_scissors.rb
+++ lib/rock_paper_scissors.rb
@@ -1,14 +1,30 @@
 class RockPaperScissors
 
-  # Exceptions this class can raise:
-  class NoSuchStrategyError < StandardError ; end
+	# Exceptions this class can raise:
+	class NoSuchStrategyError < StandardError
+		def initialize(msg = "Strategy must be one of R,P,S")
+			super msg			
+		end
+	end
 
-  def self.winner(player1, player2)
-    # YOUR CODE HERE
-  end
+	def self.winner(player1, player2)
+		table = Hash[
+			0 => [player1,player2,player1],
+			1 => [player1,player1,player2],
+			2 => [player2,player1,player1]
+		]
+		lookup = Hash['R'=>0, 'P'=>1, 'S'=>2]
 
-  def self.tournament_winner(tournament)
-    # YOUR CODE HERE
-  end
+		if !lookup.has_key?(player1.last) or !lookup.has_key?(player2.last)
+			raise NoSuchStrategyError
+		end
+
+		return table[lookup[player1.last]][lookup[player2.last]]
+	end
+
+	def self.tournament_winner(tournament)
+		return self.winner(tournament[0], tournament[1]) if tournament.flatten.size==4
+    self.winner(self.tournament_winner(tournament[0]), self.tournament_winner(tournament[1]))
+	end
 
 end
