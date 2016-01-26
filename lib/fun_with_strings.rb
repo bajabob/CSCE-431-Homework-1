d17e9528f7f2fe238f3a2a90af8f3e980bf29886
Bob Timm <bajabob.tx@gmail.com>
2016-01-25 20:52:51 -0600

    fun_with_strings test cases completed

--- lib/fun_with_strings.rb
+++ lib/fun_with_strings.rb
@@ -1,13 +1,31 @@
 module FunWithStrings
+
   def palindrome?
-    # your code here
+    word = self.downcase.scan(/\w/)
+    word == word.reverse
   end
+
   def count_words
-    # your code here
+    words = self.downcase.scan(/\w+/)
+    array = Hash.new
+    words.each do |word|
+      if word.length == 0
+        next
+      end
+      if array.has_key?(word)
+        array[word] += 1
+      else
+        array[word] = 1
+      end
+    end
+    array
   end
+
   def anagram_groups
-    # your code here
+    words = self.split
+    return words.group_by { |word| word.downcase.chars.sort }.values
   end
+
 end
 
 # make all the above functions available as instance methods on Strings:
