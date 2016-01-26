3ca44ad043784da898675cbf96c8b40b021512a3
Bob Timm <bajabob.tx@gmail.com>
2016-01-25 21:24:17 -0600

    dessert and jellybean completed

--- lib/dessert.rb
+++ lib/dessert.rb
@@ -1,17 +1,42 @@
 class Dessert
   def initialize(name, calories)
-    # your code here
+    @name=name
+    @calories=calories
   end
   def healthy?
-    # your code here
+    @calories<200
   end
   def delicious?
-    # your code here
+    @flavor != 'licorice'
   end
+
+  def name
+    @name
+  end
+  def name=(new_name)
+    @name=new_name
+  end
+
+  def calories
+    @calories
+  end
+  def calories=(new_calories)
+    @calories=new_calories
+  end
+
 end
 
 class JellyBean < Dessert
   def initialize(flavor)
-    # your code here
+    @name=flavor+" jelly bean"
+    @calories=5
+    @flavor=flavor
+  end
+
+  def flavor
+    @flavor
+  end
+  def flavor(new_flavor)
+    @flavor=new_flavor
   end
 end
